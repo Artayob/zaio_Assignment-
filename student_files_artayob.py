fd3255bd927de992105632647a81ce18f79a9952
Artayob <artayob786@gmail.com>
2025-06-16 14:54:57 +0200

    Add files via upload

--- student_files_artayob.py
+++ student_files_artayob.py
@@ -4,7 +4,7 @@ import os
 class DataLoader:
     def __init__(self, filepath):
         self.filepath = filepath
-        if not os.path.isfile(self.filepath):
+        if not os.path.exists(self.filepath):
             raise FileNotFoundError(f"File not found at: {self.filepath}")
 
     def load_content(self): # this function loads and raises the exceptions of the file 
@@ -18,3 +18,12 @@ class DataLoader:
 loader = DataLoader("C:/Users/sakit/Desktop/ZAIO_Assigment/student_habits_performance.csv")
 content = loader.load_content()
 print(content)
+
+class DataCleaner:
+    def Missing_values(self):
+        missing_values = self.load_content.check_missing_values()
+        if missing_values.any():
+            print("Missing values found at Columns:")
+        else: 
+            print("No missing values found file is good")
+        return missing_values
