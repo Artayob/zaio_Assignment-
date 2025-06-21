fd4927c34fae19761df0b652ff9294ca69616476
Artayob <artayob786@gmail.com>
2025-06-21 16:16:18 +0200

    added the visualization class

--- student_files_artayob.py
+++ student_files_artayob.py
@@ -1,5 +1,7 @@
 import pandas as pd
 import os
+import matplotlib.pyplot as plt
+import seaborn as sns
 
 class DataLoader:
     def __init__(self, filepath):
@@ -77,15 +79,50 @@ class StudentAnalyzer:
         print("The out lier values are:", outliers)
         return outliers
 
+
+class Visualizer:
+    def __init__(self, data):
+        self.data = data
+
+    def Histogram(self):
+        plt.hist(self.data["study_hours_per_day"], bins= 10, color= 'Orange', edgecolor="black")
+        plt.xlabel("Study Hours")
+        plt.ylabel("How many days")
+        plt.title("Histogram showing students study hours per day")
+        plt.grid(False)
+        plt.show()
+
+    def ScatterPlot(self):
+        plt.scatter(self.data["sleep_hours"], self.data["exam_score"], edgecolors="darkblue")
+        plt.xlabel("Sleep time")
+        plt.ylabel("Exam Scores")
+        plt.title("Scatter plot showing students sleep times compared to exam scores")
+        plt.grid(True)
+        plt.show()
+
+    def BoxPlot(self):
+        plt.figure(figsize=(8,5))
+        sns.boxplot(x ="diet_quality", y = "exam_score", data = self.data, palette="pastel")
+        plt.title("Box plot showing diet and final scores")
+        plt.xlabel("Diet Quality")
+        plt.ylabel("Final Score")
+        plt.grid(True)
+        plt.show()
+
+
 loader = DataLoader("C:/Users/sakit/Desktop/zaio_Assignment-/student_habits_performance.csv")
 content = loader.load_content()
 print(content) 
 
 analyzer = StudentAnalyzer(content)
 cleaner = DataCleaner(content)
+visualization = Visualizer(content)
 cleaner.Missing_values()
 cleaner.check_duplicates()
 cleaner.validate_ranges()
 analyzer.mean_median()
 analyzer.correlation()
 analyzer.outliers()
+visualization.Histogram()
+visualization.ScatterPlot()
+visualization.BoxPlot()
